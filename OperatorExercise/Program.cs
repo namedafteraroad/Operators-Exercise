789a7f7ea6a90a471b0c44f99ffe2aa35e9ce3fe
will street <will@williamdstreet.com>
2024-01-15 11:19:47 -0600

    first push, added exercise 1 and 2, with comments

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Original Variables for add, sub, mul
+            int a = 17;
+            int b = 4;
+
+            //Add
+            int sum = a + b;
+            
+            //Suntract
+            int difference = a - b;
+            
+            //Multiply
+            int product = a * b;
+
+
+            //____________________________________________________
+            //New variables for div and mod
+            int w = 384;
+            int s = 43;
+
+            
+            //Divide
+            int quotient = w / s;
+            
+            //Modolus
+            int remainder = w % s;
+
+            //Answers to console
+            Console.WriteLine($"{a} + {b} is {sum}.");
+            Console.WriteLine($"{a} - {b} is {difference}.");
+            Console.WriteLine($"{a} * {b} is {product}.");
+            Console.WriteLine($"{w} / {s} is {quotient} remainder {remainder}.");
+
+
+            //_____________________________________________________
+            //Exercise 2
+            //
+
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a cirlce with a radius of {radius} is {area}");
+
+
+
+
+        }
+        //New method Area of Circle, return double, add param double radius, add formula
+        public static double AreaOfCircle(double radius)
+        {
+
+            double area = Math.PI * Math.Pow( radius, 2 );
+            return area;
+
         }
     }
 }
